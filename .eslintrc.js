d6427859184d97ac9f6d62826f63fd96f4657768
Takashi Sato <takashi@tks.st>
2022-04-14 16:08:50 +0000

    feat: eslint-config-airbnb-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,48 @@
 /** @type {import('eslint').Linter.Config} */
+
+const commonRule = {
+    "no-console": "off",
+    "no-empty": ["error", { allowEmptyCatch: true }],
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+    "sort-imports": "off",
+    "import/prefer-default-export": "off",
+};
+
 module.exports = {
     root: true,
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-    plugins: ["simple-import-sort"],
     env: {
         es6: true,
         node: true,
         jest: true,
     },
-    rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "no-console": "off",
-        "no-empty": ["error", { allowEmptyCatch: true }],
-        "simple-import-sort/imports": "warn",
-        "simple-import-sort/exports": "warn",
-        "sort-imports": "off",
-    },
+    plugins: ["simple-import-sort"],
     overrides: [
         {
             files: "*.js",
+            extends: ["eslint:recommended", "airbnb-base", "prettier"],
             rules: {
-                "simple-import-sort/imports": "off",
-                "simple-import-sort/exports": "off",
+                ...commonRule,
+            },
+        },
+        {
+            files: "*.ts",
+            extends: [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/recommended",
+                "airbnb-base",
+                "airbnb-typescript/base",
+                "prettier",
+            ],
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+            rules: {
+                "@typescript-eslint/explicit-function-return-type": "off",
+                "@typescript-eslint/no-empty-function": "off",
+                "@typescript-eslint/no-use-before-define": "off",
+                "@typescript-eslint/no-explicit-any": "off",
+                ...commonRule,
             },
         },
     ],
