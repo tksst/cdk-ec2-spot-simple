18add1237ebc27e8245ffaae9c29dc68e7fe75ab
Takashi Sato <takashi@tks.st>
2022-04-16 06:41:26 +0000

    feat: move prettier configuration to @tksst/prettier-config

--- prettier.config.js
+++ prettier.config.js
@@ -1,20 +1,5 @@
 /** @type {import('prettier').Options} */
 module.exports = {
-    tabWidth: 4,
-    printWidth: 120,
-    overrides: [
-        {
-            files: "*.json",
-            options: {
-                tabWidth: 2,
-            },
-        },
-        {
-            files: "*.{md,yml}",
-            options: {
-                tabWidth: 2,
-                trailingComma: "none",
-            },
-        },
-    ],
+    // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+    ...require("@tksst/prettier-config"),
 };
