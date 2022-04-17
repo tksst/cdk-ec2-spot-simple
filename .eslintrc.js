b72aa9cd222d466b725121355fa963b90e7735e8
Takashi Sato <takashi@tks.st>
2022-04-17 16:09:01 +0000

    feat: move ESLint configuration to @tksst/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,16 @@
 /** @type {import('eslint').Linter.Config} */
 
-const commonRule = {
-    "no-console": "off",
-    "no-empty": ["error", { allowEmptyCatch: true }],
-    "simple-import-sort/imports": "warn",
-    "simple-import-sort/exports": "warn",
-    "sort-imports": "off",
-    "import/prefer-default-export": "off",
-};
-
 module.exports = {
     root: true,
     env: {
-        es6: true,
+        // Your project environment settings here.
+        // See ESLint document of "Specifying Environments":
+        // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+
+        // example:
+        es2020: true,
         node: true,
         jest: true,
     },
-    plugins: ["simple-import-sort"],
-    overrides: [
-        {
-            files: "*.js",
-            extends: ["eslint:recommended", "airbnb-base", "prettier"],
-            rules: {
-                ...commonRule,
-            },
-        },
-        {
-            files: "*.ts",
-            extends: [
-                "eslint:recommended",
-                "plugin:@typescript-eslint/recommended",
-                "airbnb-base",
-                "airbnb-typescript/base",
-                "prettier",
-            ],
-            parserOptions: {
-                project: "./tsconfig.json",
-            },
-            rules: {
-                "@typescript-eslint/explicit-function-return-type": "off",
-                "@typescript-eslint/no-empty-function": "off",
-                "@typescript-eslint/no-use-before-define": "off",
-                "@typescript-eslint/no-explicit-any": "off",
-                ...commonRule,
-            },
-        },
-    ],
+    extends: "@tksst",
 };
