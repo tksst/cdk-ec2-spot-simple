14f38d187f458a39296bfa6f151d20b78b9b53fa
Takashi Sato <takashi@tks.st>
2022-07-16 20:05:31 +0900

    remove NOTICE message from src/bin/index.ts
    The source code has no originality as a copyrighted work.

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-// This file is from
-// https://github.com/matzkoh/typescript-npm-starter
-// That says it is licensed under MIT.
-
 // eslint-disable-next-line import/extensions
 import "source-map-support/register.js";
 
