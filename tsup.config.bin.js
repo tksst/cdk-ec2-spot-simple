0a14ad3c4329075052d7cf57f5891bf5169e0afa
Takashi Sato <takashi@tks.st>
2022-07-24 00:55:05 +0900

    extract tsup config to typescript-starter

---
+++ tsup.config.bin.js
@@ -0,0 +1,7 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+
+// eslint-disable-next-line import/extensions
+import { binOptions } from "@tksst/typescript-starter/tsup-config.mjs";
+import { defineConfig } from "tsup";
+
+export default defineConfig(binOptions);
---
+++ tsup.config.lib.js
@@ -0,0 +1,7 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+
+// eslint-disable-next-line import/extensions
+import { libOptions } from "@tksst/typescript-starter/tsup-config.mjs";
+import { defineConfig } from "tsup";
+
+export default defineConfig(libOptions);
