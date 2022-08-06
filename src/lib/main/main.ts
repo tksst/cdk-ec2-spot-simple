add234b9e0897167a43e91a8e55de297cec2907b
Takashi Sato <takashi@tks.st>
2022-08-06 08:47:28 +0000

    remove reduntunt license declaration

--- src/lib/main/main.ts
+++ src/lib/main/main.ts
@@ -1,8 +1,3 @@
-/**
- * These codes are licensed under CC0.
- * http://creativecommons.org/publicdomain/zero/1.0/deed.ja
- */
-
 export const main = (): void => {
     console.log("This is a template for Node.js CLI or Library project in TypeScript.");
 };
