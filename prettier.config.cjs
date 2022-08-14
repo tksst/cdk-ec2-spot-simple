d3d13a747cbc4759c58a488e5f7bcfc3b56a59c0
Takashi Sato <takashi@tks.st>
2022-08-14 18:46:12 +0900

    remove unneeded eslint-disable-next-line

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('prettier').Options} */
 module.exports = {
-    // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+    // eslint-disable-next-line import/no-extraneous-dependencies
     ...require("@tksst/prettier-config"),
 };
