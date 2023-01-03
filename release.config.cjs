25bd12037878beff3f353f303a0633707ebcece9
Takashi Sato <takashi@tks.st>
2023-01-03 17:56:24 +0900

    ci: fix a bug that pre-release is released with a wrong version number

--- release.config.cjs
+++ release.config.cjs
@@ -6,17 +6,13 @@ const pluginsRelease = [
     "@semantic-release/changelog",
     ["@semantic-release/npm", { npmPublish: false }],
     "@semantic-release/github",
+    // jsii-pacmak reads our version from package.json, so semantic-release should update package.json.
     "@semantic-release/git",
     ["@semantic-release/exec", { publishCmd: "pnpm exec publib dist-jsii" }],
 ];
 
-const plubinsPreRelease = [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", { npmPublish: false }],
-    "@semantic-release/github",
-    ["@semantic-release/exec", { publishCmd: "pnpm exec publib dist-jsii" }],
-];
+// CHANGELOG.md should not be updated on pre-release because it will conflict with a future release.
+const plubinsPreRelease = pluginsRelease.filter((it) => it !== "@semantic-release/changelog");
 
 const config = {
     branches: ["master", { name: "alpha", prerelease: true }],
