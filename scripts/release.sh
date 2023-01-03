7de2f8e60d14b8ded75305a47321f6d8071cfad3
Takashi Sato <takashi@tks.st>
2023-01-04 01:10:42 +0900

    ci: fixed a bug where the wrong version was released

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o posix
+
+version=$1
+
+oldfile=$( mktemp ./old-package.json-XXXXXXXXX )
+
+mv package.json "$oldfile"
+
+trap 'mv "$oldfile" package.json' EXIT
+
+# replace "version" of package.json
+jq ".version|=\"$version\"" "$oldfile" > package.json
+
+pnpm exec publib dist-jsii
